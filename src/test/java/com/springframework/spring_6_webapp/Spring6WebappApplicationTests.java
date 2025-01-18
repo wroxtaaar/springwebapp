aad2e874a1d572bdbd1b5b3d095a60acd62dd11e
wasiq <wroxtaar@gmail.com>
2025-01-18 11:35:45 +0530

    spring guru

---
+++ src/test/java/com/springframework/spring_6_webapp/Spring6WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.spring_6_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
